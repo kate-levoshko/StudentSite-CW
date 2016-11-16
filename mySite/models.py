5bddee04a0e06c68697d2282076343e761ed5d3f
kate-levoshko <klevoshko98@gmail.com>
2016-11-16 21:47:41 +0200

    data base works

---
+++ mySite/models.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+"""
+class Faculty(models.Model):
+    name = models.CharField(max_length=200)
+    use = models.CharField(max_length=20)
+"""
+
