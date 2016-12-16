3f514620e5e6d2a085ed0cb786cfd359fd6ea22a
kate-levoshko <klevoshko98@gmail.com>
2016-12-16 02:35:45 +0200

    Prepare for deploying

--- 2/mysite/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from mysite.models import Helper
-
-class HelperAdmin(admin.ModelAdmin):
-    fields = ['faculty_name', 'teacher', 'discipline_name', 'discipline_year', 'upload_material']
-
-admin.site.register(Helper, HelperAdmin)
